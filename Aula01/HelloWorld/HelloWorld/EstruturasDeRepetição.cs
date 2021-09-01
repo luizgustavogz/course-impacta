5787a998485b9e136d23c53d7decd299ebb66cbd
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2021-09-01 17:55:34 -0300

    Estruturas de repetição

---
+++ Aula01/HelloWorld/HelloWorld/EstruturasDeRepetição.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class EstruturasDeRepetição
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i < args.Length; i++)
+            {
+                var argumento = args[i];
+                Console.WriteLine($"Argumento lido no for: {argumento}");
+            }
+
+            Console.WriteLine();
+            foreach(var argumento in args)
+            {
+                Console.WriteLine($"Argumento lido no foreach: {argumento}");
+            }
+
+            Console.WriteLine();
+            var j = 0;
+            while (j < args.Length)
+            {
+                var argumento = args[j];
+                Console.WriteLine($"Argumento lido no while: {argumento}");
+                j++;
+            }
+
+            Console.WriteLine();
+            var n = 0;
+            do
+            {
+                var argumento = args[n];
+                Console.WriteLine($"Argumento lido no do while: {argumento}");
+                n++;
+            } while (n < args.Length);
+        }
+    }
+}
--- Aula01/HelloWorld/HelloWorld/HelloWorld.cs
+++ Aula01/HelloWorld/HelloWorld/HelloWorld.cs
@@ -4,17 +4,28 @@ namespace HelloWorld
 {
     class HelloWorld
     {
+        /*
         static void Main(string[] args)
         {
             bool isAdulto = true;
             decimal valor = 10.55m;
             double valor2 = 10.55;
             char sexo = 'M';
-
             var nome = "Luiz Gustavo";
             var idade = 19;
-
-            Console.WriteLine($"{nome}, sexo: {sexo}, {idade} anos");
+            var maior = "";
+         
+            if(idade >= 18)
+            {
+                maior = "maior de idade";
+            }
+            else
+            {
+                maior = "menor de idade"; 
+            }
+            
+            Console.WriteLine($"{nome}, sexo: {sexo}\n{idade} anos, {maior}");
         }
+        */
     }
 }
