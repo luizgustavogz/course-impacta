20621a1a91ea1dbdb6479dee9396a176a6b88418
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2021-09-14 16:29:23 -0300

    TesteDapper

---
+++ Aula07/TesteDapper/TesteDapper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TesteDapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
